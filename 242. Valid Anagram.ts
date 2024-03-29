f90582a54b1ba4621c3a4638c26601e6eccf66a0
Nick <maniutin@gmail.com>
2024-03-29 12:28:00 -0400

    refactor 242. Valid Anagram

--- 242. Valid Anagram.ts
+++ 242. Valid Anagram.ts
@@ -1,10 +1,3 @@
 function isAnagram(s: string, t: string): boolean {
-  const sortedS = s.split("").sort();
-  const sortedT = t.split("").sort();
-  
-  if (sortedS.toString() === sortedT.toString()) {
-      return true;
-  } else {
-      return false;
-  };
-};
+  return s.split("").sort().join(",") === t.split("").sort().join(",");
+}
