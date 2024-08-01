3412391050776ba1ba1404c0df2331a0ac4d16a9
Nick <maniutin@gmail.com>
2024-08-01 13:35:17 -0400

    add frequency array solution

--- 49. Group Anagrams.ts
+++ 49. Group Anagrams.ts
@@ -2,32 +2,35 @@ function groupAnagrams(strs: string[]): string[][] {
   // own solution
   const tempArr = [];
 
-    const hashMap = {};
+  const hashMap = {};
 
-    for (const str in strs){
-        const localTemp = []
-        for (const stragn in strs){
-            if (strs[str].split("").sort().toString() === strs[stragn].split("").sort().toString()){
-                if(localTemp.indexOf(strs[stragn]) === -1) {
-                    localTemp.push(strs[stragn]);
-                }
-            tempArr.push(localTemp);
-            }
+  for (const str in strs) {
+    const localTemp = [];
+    for (const stragn in strs) {
+      if (
+        strs[str].split("").sort().toString() ===
+        strs[stragn].split("").sort().toString()
+      ) {
+        if (localTemp.indexOf(strs[stragn]) === -1) {
+          localTemp.push(strs[stragn]);
         }
+        tempArr.push(localTemp);
+      }
     }
+  }
 
-    tempArr.forEach(function(arr){
-        hashMap[arr.join("|")] = arr;
-    });
+  tempArr.forEach(function (arr) {
+    hashMap[arr.join("|")] = arr;
+  });
 
-    const result = Object.keys(hashMap).map(function(k){
-        return hashMap[k]
-    })
+  const result = Object.keys(hashMap).map(function (k) {
+    return hashMap[k];
+  });
 
-    return result;
+  return result;
 
-    // better solution
-    /*  let map = {}
+  // better solution
+  /*  let map = {}
 
      for (let str of strs) {
          let s = str.split('').sort().join('');
@@ -36,4 +39,27 @@ function groupAnagrams(strs: string[]): string[][] {
      }
 
      return Object.values(map); */
-};
+
+  // using frequency array
+
+  /* const res = {};
+
+    for (let str of strs) {
+        let charFreq = Array(26).fill(0);
+
+        for (let i = 0; i < str.length; i++) {
+            charFreq[str.charCodeAt(i) - 97]++;
+        };
+
+        let keyStr = charFreq.toString();
+
+        if (!res[keyStr]) {
+            res[keyStr] = [];
+        };
+
+        res[keyStr].push(str);
+
+    }
+
+    return Array.from(Object.values(res)); */
+}
