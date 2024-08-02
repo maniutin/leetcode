af97a24a97c472f743df3f1e8e2760816860333c
Nick <maniutin@gmail.com>
2024-08-02 13:20:06 -0400

    better solution for 167.

--- 167. Two Sum II - Input Array Is Sorted.ts
+++ 167. Two Sum II - Input Array Is Sorted.ts
@@ -1,9 +1,12 @@
 function twoSum(numbers: number[], target: number): number[] {
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-      if (numbers[j] == target - numbers[i]) {
-        return [i + 1, j + 1];
-      }
-    }
+  let low = 0;
+  let high = numbers.length - 1;
+
+  while (low <= high) {
+    if (numbers[low] + numbers[high] > target) high--;
+    else if (numbers[low] + numbers[high] < target) low++;
+    else return [low + 1, high + 1];
   }
+
+  return [];
 }
